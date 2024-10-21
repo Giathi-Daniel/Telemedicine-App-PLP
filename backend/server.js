3d610c0f6ba2bc1e225fea07b9eadb919f47d8b1
Daniel Giathi <blacksharkchi@proton.me>
2024-10-21 06:13:26 +0300

    add comments

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
-const db = require('./config/db')
-const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const MySQLStore = require('express-mysql-session')('session')
-require('dotenv').config()
+// import neccessary packages
+const db = require('./config/db') // database conn
+const express = require('express') // web server
+const bodyParser = require('body-parser') // capture form data
+const session = require('express-session') // session-management
+const MySQLStore = require('express-mysql-session')('session') // session-management storage
+require('dotenv').config() // manage environment variables
 
 const app = express()
 
 // middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // capture form data
+
+// sesssion store
+const sessionStore = new MySQLStore({}, db)
+
+// configure session middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveuninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 // 1 hour
+
+    }
+}))
 
 app.listen(3000, console.log('Server running'))
