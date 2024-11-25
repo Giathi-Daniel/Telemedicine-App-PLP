87268999a1aad0170dc6c2e64d53ea4eec236e52
Daniel Giathi <127479628+Giathi-Daniel@users.noreply.github.com>
2024-11-25 19:21:46 +0300

    Update patientRoutes.js

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -18,7 +18,7 @@ router.post(
 
 router.post('/login', loginUser);
 
-// Only patients should access their profile
+// Only patients access their profile
 router.get('/profile', checkRole('patient'), getUser);
 
 // Edit patient details
